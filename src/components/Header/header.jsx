5cfccbaabc7b1ddfef812d6532802414b81e1ae1
JoaoBrguin <joaopedromoraes76@gmail.com>
2025-10-03 15:46:09 -0300

    começo

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,21 @@
+import style from './Header.module.css'
+import { BsCodeSlash } from "react-icons/bs";
+
+export default function Header() {
+    return (
+        <>
+            <div className={style.header}>
+                <div className={style.name}>
+                    <BsCodeSlash /><h1>Moraes</h1>
+                </div>
+                <nav>
+                    <a href="#">Teste</a>
+                    <a href="#">Teste</a>
+                    <a href="#">Teste</a>
+                    <a href="#">Teste</a>
+                </nav>
+            </div>
+
+        </>
+    )
+}
