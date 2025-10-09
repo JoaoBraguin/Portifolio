96d44283349342c260d177be470e2baad46abd70
JoaoBrguin <joaopedromoraes76@gmail.com>
2025-10-09 15:48:06 -0300

    hero feito

---
+++ src/components/Habilidades/Habilidades.jsx
@@ -0,0 +1,19 @@
+import { FaReact } from "react-icons/fa";
+
+
+export default function Habiliades() {
+    <>
+        <div>
+            <h1>Habilidades</h1>
+        </div>
+        <div>
+            <div>
+                <FaReact />
+                <h1>React</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa esse quo iste saepe qui quia a provident perspiciatis tenetur numquam, quae nihil non neque atque, corrupti nam quam. Rerum, dignissimos?</p>
+            </div>
+        </div>
+
+
+    </>
+}
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,12 +1,13 @@
 import style from './Header.module.css'
 import { BsCodeSlash } from "react-icons/bs";
+import { BsFillLightningChargeFill } from "react-icons/bs";
 
 export default function Header() {
     return (
         <>
             <div className={style.header}>
                 <div className={style.name}>
-                    <BsCodeSlash /><h1>Moraes</h1>
+                    <BsFillLightningChargeFill />  <h1>Moraes</h1>
                 </div>
                 <nav>
                     <a href="#">Teste</a>
